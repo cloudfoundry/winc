50b02b29dbeb92fd0e41feb87164429e0b94fccc
Sam Smith <sesmith177@gmail.com>
2017-11-28 10:22:05 -0500

    Don't use powershell to set network interface MTU
    - Use GetIpInterfaceEntry / SetIpInterfaceEntry
    - Note that these functions need to be executed in the network
    compartmanet corresponding with that container
    
    [#152887346]
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- netrules/netinterface/net_interface.go
+++ netrules/netinterface/net_interface.go
@@ -3,10 +3,107 @@ package netinterface
 import (
 	"fmt"
 	"net"
-	"os/exec"
+	"os"
+	"runtime"
+	"syscall"
+	"unsafe"
+
+	"github.com/Microsoft/hcsshim"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/windows"
 )
 
 type NetInterface struct{}
+
+var (
+	iphlpapi            = windows.NewLazySystemDLL("iphlpapi.dll")
+	getIpInterfaceEntry = iphlpapi.NewProc("GetIpInterfaceEntry")
+	setIpInterfaceEntry = iphlpapi.NewProc("SetIpInterfaceEntry")
+)
+
+// https://msdn.microsoft.com/en-us/library/windows/desktop/aa365915(v=vs.85).aspx
+const GAA_FLAG_INCLUDE_ALL_COMPARTMENTS = 0x200
+
+// https://msdn.microsoft.com/en-us/library/windows/desktop/aa366320(v=vs.85).aspx
+type NET_LUID struct {
+	Value uint64
+}
+
+// https://msdn.microsoft.com/en-us/library/windows/desktop/aa814496(v=vs.85).aspx
+type MIB_IPINTERFACE_ROW struct {
+	Family                               uint32
+	InterfaceLuid                        NET_LUID
+	InterfaceIndex                       uint32
+	MaxReassemblySize                    uint32
+	InterfaceIdentifier                  uint64
+	MinRouterAdvertisementInterval       uint32
+	MaxRouterAdvertisementInterval       uint32
+	AdvertisingEnabled                   bool
+	ForwardingEnabled                    bool
+	WeakHostSend                         bool
+	WeakHostReceive                      bool
+	UseAutomaticMetric                   bool
+	UseNeighborUnreachabilityDetection   bool
+	ManagedAddressConfigurationSupported bool
+	OtherStatefulConfigurationSupported  bool
+	AdvertiseDefaultRoute                bool
+	RouterDiscoveryBehavior              uint32
+	DadTransmits                         uint32
+	BaseReachableTime                    uint32
+	RetransmitTime                       uint32
+	PathMtuDiscoveryTimeout              uint32
+	LinkLocalAddressBehavior             uint32
+	LinkLocalAddressTimeout              uint32
+	ZoneIndices                          [16]uint32
+	SitePrefixLength                     uint32
+	Metric                               uint32
+	NlMtu                                uint32
+	Connected                            bool
+	SupportsWakeUpPatterns               bool
+	SupportsNeighborDiscovery            bool
+	SupportsRouterDiscovery              bool
+	ReachableTime                        uint32
+	TransmitOffload                      uint16
+	ReceiveOffload                       uint16
+	DisableDefaultRoutes                 bool
+}
+
+// This struct is defined in the Go stdlib. However, that definition doesn't go
+// up to the CompartmentId, which we need.
+// https://msdn.microsoft.com/en-us/library/windows/desktop/aa366058(v=vs.85).aspx
+
+type IP_ADAPTER_ADDRESSES struct {
+	Length                 uint32
+	IfIndex                uint32
+	Next                   *IP_ADAPTER_ADDRESSES
+	AdapterName            *byte
+	FirstUnicastAddress    *windows.IpAdapterUnicastAddress
+	FirstAnycastAddress    *windows.IpAdapterAnycastAddress
+	FirstMulticastAddress  *windows.IpAdapterMulticastAddress
+	FirstDnsServerAddress  *windows.IpAdapterDnsServerAdapter
+	DnsSuffix              *uint16
+	Description            *uint16
+	FriendlyName           *uint16
+	PhysicalAddress        [syscall.MAX_ADAPTER_ADDRESS_LENGTH]byte
+	PhysicalAddressLength  uint32
+	Flags                  uint32
+	Mtu                    uint32
+	IfType                 uint32
+	OperStatus             uint32
+	Ipv6IfIndex            uint32
+	ZoneIndices            [16]uint32
+	FirstPrefix            *windows.IpAdapterPrefix
+	TransmitLinkSpeed      uint64
+	ReceiveLinkSpeed       uint64
+	FirstWinsServerAddress uint64
+	FirstGatewayAddress    uint64
+	Ipv4Metric             uint32
+	Ipv6Metric             uint32
+	Luid                   NET_LUID
+	Dhcpv4Server           windows.SocketAddress
+	CompartmentId          uint32
+	/* more fields follow */
+}
 
 func (n *NetInterface) ByName(name string) (*net.Interface, error) {
 	return net.InterfaceByName(name)
@@ -41,9 +138,74 @@ func (n *NetInterface) ByIP(ipStr string) (*net.Interface, error) {
 }
 
 func (n *NetInterface) SetMTU(alias string, mtu int) error {
-	output, err := exec.Command("powershell.exe", "-command", fmt.Sprintf(`$index = (Get-NetIPInterface -Includeallcompartments -interfaceAlias "%s").ifIndex; set-netipinterface -includeallcompartments -ifindex $index -nlmtubytes %d`, alias, mtu)).CombinedOutput()
+	luid, compartmentID, err := getLuidAndCompartment(alias)
 	if err != nil {
-		return fmt.Errorf("unable to set MTU. \n error: %s \n output: %s", err.Error(), string(output))
+		return err
 	}
+
+	runtime.LockOSThread()
+	defer func() {
+		hcsshim.SetCurrentThreadCompartmentId(0)
+		runtime.UnlockOSThread()
+	}()
+	if err := hcsshim.SetCurrentThreadCompartmentId(compartmentID); err != nil {
+		logrus.Error(err)
+		return err
+	}
+
+	var row MIB_IPINTERFACE_ROW
+	row.InterfaceLuid = luid
+	row.Family = windows.AF_INET
+
+	r0, _, err := syscall.Syscall(getIpInterfaceEntry.Addr(), 1, uintptr(unsafe.Pointer(&row)), 0, 0)
+	if int32(r0) != 0 {
+		err := fmt.Errorf("GetIpInterfaceEntry: 0x%x", r0)
+		logrus.Error(err)
+		return err
+	}
+
+	row.NlMtu = uint32(mtu)
+
+	// From https://msdn.microsoft.com/en-us/library/windows/desktop/aa814465(v=vs.85).aspx
+	// SitePrefixLength must be 0 for IPv4 interfaces
+	row.SitePrefixLength = 0
+
+	r0, _, err = syscall.Syscall(setIpInterfaceEntry.Addr(), 1, uintptr(unsafe.Pointer(&row)), 0, 0)
+	if int32(r0) != 0 {
+		err := fmt.Errorf("SetIpInterfaceEntry: 0x%x", r0)
+		logrus.Error(err)
+		return err
+	}
+
 	return nil
+}
+
+func getLuidAndCompartment(alias string) (NET_LUID, uint32, error) {
+	var b []byte
+	l := uint32(15000)
+	for {
+		b = make([]byte, l)
+		err := windows.GetAdaptersAddresses(syscall.AF_UNSPEC, windows.GAA_FLAG_INCLUDE_PREFIX|GAA_FLAG_INCLUDE_ALL_COMPARTMENTS, 0, (*windows.IpAdapterAddresses)(unsafe.Pointer(&b[0])), &l)
+		if err == nil {
+			if l == 0 {
+				return NET_LUID{}, 0, nil
+			}
+			break
+		}
+		if err.(syscall.Errno) != syscall.ERROR_BUFFER_OVERFLOW {
+			return NET_LUID{}, 0, os.NewSyscallError("getadaptersaddresses", err)
+		}
+		if l <= uint32(len(b)) {
+			return NET_LUID{}, 0, os.NewSyscallError("getadaptersaddresses", err)
+		}
+	}
+
+	for aa := (*IP_ADAPTER_ADDRESSES)(unsafe.Pointer(&b[0])); aa != nil; aa = aa.Next {
+		name := syscall.UTF16ToString((*(*[10000]uint16)(unsafe.Pointer(aa.FriendlyName)))[:])
+		if name == alias {
+			return aa.Luid, aa.CompartmentId, nil
+		}
+	}
+
+	return NET_LUID{}, 0, fmt.Errorf("interface %s not found", alias)
 }
