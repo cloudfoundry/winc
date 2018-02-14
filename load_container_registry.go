4617e0fcc1e3e1caa0b6d9707f17f6c91dd57198
Sam Smith <sesmith177@gmail.com>
2018-02-14 18:01:23 -0500

    WIP, can write a key
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- load_container_registry.go
+++ load_container_registry.go
@@ -20,7 +20,8 @@ var (
 	orCloseHive    = offreg.NewProc("ORCloseHive")
 	orOpenKey      = offreg.NewProc("OROpenKey")
 	orQueryInfoKey = offreg.NewProc("ORQueryInfoKey")
-	orEnumKey      = offreg.NewProc("OREnumKey")
+	orEnumValue    = offreg.NewProc("OREnumValue")
+	orSetValue     = offreg.NewProc("ORSetValue")
 	orCloseKey     = offreg.NewProc("ORCloseKey")
 )
 
@@ -62,77 +63,109 @@ func main() {
 }
 
 func doRegistryStuff(root syscall.Handle) error {
-	var numSubKeys uint32
-	var maxSubKeyLen uint32
+	var numValues uint32
+	var maxValueNameLen uint32
+	var maxValueDataLen uint32
 
-	r0, _, _ := orQueryInfoKey.Call(
-		uintptr(root),
+	path := `ControlSet001\Services\HTTP\Parameters\UrlAclInfo`
+	handle, err := openKey(root, path)
+	if err != nil {
+		return err
+	}
+	defer closeKey(handle)
+
+	valueName := "http://sams.cool.website:4444/"
+	vn, err := syscall.UTF16PtrFromString(valueName)
+	if err != nil {
+		return nil
+	}
+
+	data := []byte{0xd, 0xe, 0xa, 0xd, 0xb, 0xe, 0xe, 0xf}
+
+	r0, _, _ := orSetValue.Call(
+		uintptr(handle),
+		uintptr(unsafe.Pointer(vn)),
+		uintptr(windows.REG_BINARY),
+		uintptr(unsafe.Pointer(&data[0])),
+		uintptr(8),
+	)
+
+	if r0 != 0 {
+		return fmt.Errorf("ORSetKey: %s\n", windowsErrorMessage(uint32(r0)))
+	}
+
+	r0, _, _ = orQueryInfoKey.Call(
+		uintptr(handle),
 		uintptr(0),
 		uintptr(0),
-		uintptr(unsafe.Pointer(&numSubKeys)),   //		  _Out_opt_   PDWORD    lpcSubKeys,
-		uintptr(unsafe.Pointer(&maxSubKeyLen)), //  _Out_opt_   PDWORD    lpcMaxSubKeyLen,
-		uintptr(0),                             //  _Out_opt_   PDWORD    lpcMaxClassLen,
-		uintptr(0),                             //  _Out_opt_   PDWORD    lpcValues,
-		uintptr(0),                             //  _Out_opt_   PDWORD    lpcMaxValueNameLen,
-		uintptr(0),                             //  _Out_opt_   PDWORD    lpcMaxValueLen,
-		uintptr(0),                             //  _Out_opt_   PDWORD    lpcbSecurityDescriptor,
-		uintptr(0),                             //  _Out_opt_   PFILETIME lpftLastWriteTime
+		uintptr(0),                                //		  _Out_opt_   PDWORD    lpcSubKeys,
+		uintptr(0),                                //  _Out_opt_   PDWORD    lpcMaxSubKeyLen,
+		uintptr(0),                                //  _Out_opt_   PDWORD    lpcMaxClassLen,
+		uintptr(unsafe.Pointer(&numValues)),       //  _Out_opt_   PDWORD    lpcValues,
+		uintptr(unsafe.Pointer(&maxValueNameLen)), //  _Out_opt_   PDWORD    lpcMaxValueNameLen,
+		uintptr(unsafe.Pointer(&maxValueDataLen)), //  _Out_opt_   PDWORD    lpcMaxValueLen,
+		uintptr(0), //  _Out_opt_   PDWORD    lpcbSecurityDescriptor,
+		uintptr(0), //  _Out_opt_   PFILETIME lpftLastWriteTime
 	)
 
 	if r0 != 0 {
 		return fmt.Errorf("ORQueryInfoKey: %s\n", windowsErrorMessage(uint32(r0)))
 	}
 
-	fmt.Println(numSubKeys)
-	fmt.Println(maxSubKeyLen)
+	fmt.Println(numValues)
+	fmt.Println(maxValueNameLen)
+	fmt.Println(maxValueDataLen)
+	valueNameBuf := make([]uint16, maxValueNameLen+1)
 
-	foo := uint32(10000)
+	for i := uint32(0); i < numValues; i++ {
+		size := maxValueNameLen + 1
 
-	for i := uint32(0); i < numSubKeys; i++ {
-		keyNameBuf := make([]uint16, 10000)
-
-		r0, _, _ = orEnumKey.Call(
-			uintptr(root),
+		r0, _, _ = orEnumValue.Call(
+			uintptr(handle),
 			uintptr(i),
-			uintptr(unsafe.Pointer(&keyNameBuf[0])),
-			uintptr(unsafe.Pointer(&foo)),
+			uintptr(unsafe.Pointer(&valueNameBuf[0])),
+			uintptr(unsafe.Pointer(&size)),
 			uintptr(0),
 			uintptr(0),
 			uintptr(0),
 		)
 
 		if r0 != 0 {
-			return fmt.Errorf("OREnumKey: %s\n", windowsErrorMessage(uint32(r0)))
+			return fmt.Errorf("OREnumValue: %s\n", windowsErrorMessage(uint32(r0)))
 		}
 
-		fmt.Println(syscall.UTF16ToString(keyNameBuf))
-
+		fmt.Println(syscall.UTF16ToString(valueNameBuf))
 	}
 
-	//pathKey := `HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment`
-	//p, err := syscall.UTF16PtrFromString(pathKey)
-	//if err != nil {
-	//	panic(err)
-	//}
+	return nil
+}
 
-	//	var env syscall.Handle
-	//	r0, _, _ = orOpenKey.Call(
-	//		uintptr(unsafe.Pointer(root)),
-	//		uintptr(unsafe.Pointer(p)),
-	//		uintptr(unsafe.Pointer(&env)),
-	//	)
-	//	if r0 != 0 {
-	//		fmt.Printf("OROpenKey: %s\n", windowsErrorMessage(uint32(r0)))
-	//		return
-	//	}
-	//
-	//	r0, _, _ = orCloseKey.Call(
-	//		uintptr(unsafe.Pointer(env)),
-	//	)
-	//	if r0 != 0 {
-	//		fmt.Printf("ORCloseKey: %s\n", windowsErrorMessage(uint32(r0)))
-	//		return
-	//	}
+func openKey(handle syscall.Handle, subKeyName string) (syscall.Handle, error) {
+	p, err := syscall.UTF16PtrFromString(subKeyName)
+	if err != nil {
+		return 0, err
+	}
+
+	var key syscall.Handle
+	r0, _, _ := orOpenKey.Call(
+		uintptr(unsafe.Pointer(handle)),
+		uintptr(unsafe.Pointer(p)),
+		uintptr(unsafe.Pointer(&key)),
+	)
+	if r0 != 0 {
+		return 0, fmt.Errorf("OROpenKey: %s\n", windowsErrorMessage(uint32(r0)))
+	}
+
+	return key, nil
+}
+
+func closeKey(handle syscall.Handle) error {
+	r0, _, _ := orCloseKey.Call(
+		uintptr(unsafe.Pointer(handle)),
+	)
+	if r0 != 0 {
+		return fmt.Errorf("ORCloseKey: %s\n", windowsErrorMessage(uint32(r0)))
+	}
 
 	return nil
 }
