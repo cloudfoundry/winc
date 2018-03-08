df6aed589dc7ca503c6128c7427dee9a75d9ae14
Sam Smith <sesmith177@gmail.com>
2018-03-08 17:40:18 -0500

    WIP
    Signed-off-by: Natalie Arellano <narellano@pivotal.io>

--- integration/winc/run_test.go
+++ integration/winc/run_test.go
@@ -39,7 +39,8 @@ var _ = Describe("Run", func() {
 		bundleSpec.Mounts = []specs.Mount{{Source: mountSrc, Destination: "C:\\mountSomewhereElse"}}
 		Expect(ioutil.WriteFile(filepath.Join(mountSrc, "hi.txt"), []byte("hello"), 0644)).To(Succeed())
 		Expect(acl.Apply(mountSrc, false, false, acl.GrantName(windows.GENERIC_ALL, "Everyone"))).To(Succeed())
-		bundleSpec.Process.Args = []string{filepath.Join("c:\\mountSomewhereElse", "sleep.exe")}
+		//		bundleSpec.Process.Args = []string{filepath.Join("c:\\mountSomewhereElse", "sleep.exe")}
+		bundleSpec.Process.Args = []string{filepath.Join("sleep.exe")}
 		fmt.Println(mountSrc)
 	})
 
