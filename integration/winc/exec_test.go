f8f5c8f186be59be962fc27bd1634e8335c82882
Amin Jamali <ajamali@pivotal.io>
2018-07-23 12:14:52 -0400

    WIP- repeat the create_test in exec_test
    Signed-off-by: Akshat Gokhale <agokhale@pivotal.io>

--- integration/winc/exec_test.go
+++ integration/winc/exec_test.go
@@ -34,7 +34,7 @@ var _ = Describe("Exec", func() {
 			bundlePath, err = ioutil.TempDir("", "winccontainer")
 			Expect(err).To(Succeed())
 
-			Expect(ioutil.WriteFile(filepath.Join(filepath.dir(sleepBin), "sentinel"), []byte("hello"), 0644)).To(Succeed())
+			Expect(ioutil.WriteFile(filepath.Join(filepath.Dir(sleepBin), "sentinel"), []byte("hello"), 0644)).To(Succeed())
 			containerId = filepath.Base(bundlePath)
 
 			bundleSpec = helpers.GenerateRuntimeSpec(helpers.CreateVolume(rootfsURI, containerId))
@@ -45,6 +45,7 @@ var _ = Describe("Exec", func() {
 
 		AfterEach(func() {
 			failed = failed || CurrentGinkgoTestDescription().Failed
+			fmt.Printf("ContainerID: %s\n", containerId)
 			//helpers.DeleteContainer(containerId)
 			//helpers.DeleteVolume(containerId)
 			Expect(os.RemoveAll(bundlePath)).To(Succeed())
