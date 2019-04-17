95b8f30aa30fc7e731eee8c62163e4a9b434312b
Garden Windows <garden-windows-eng@pivotal.io>
2019-04-17 16:40:16 -0400

    Revert "Verify that we get accurate memory stats from hcs client."
    This reverts commit 026425ae3b784fad022450cbbd9ccafd2b1b73f3.
    
    - This has increased the frequency of the flake.
    
    [#164996755]

--- integration/winc/events_test.go
+++ integration/winc/events_test.go
@@ -60,10 +60,9 @@ var _ = Describe("Events", func() {
 
 				It("prints the container memory stats to stdout", func() {
 					stats := getStats(containerId)
-					initialRss := stats.Data.Memory.Stats.TotalRss
-					Expect(initialRss).To(BeNumerically(">", 0))
+					Expect(stats.Data.Memory.Stats.TotalRss).To(BeNumerically(">", 0))
 
-					memConsumedBytes := 1024
+					memConsumedBytes := 600 * 1024 * 1024
 
 					cmd := exec.Command(wincBin, "exec", containerId, "c:\\consume.exe", strconv.Itoa(memConsumedBytes), "10")
 					stdOut, err := cmd.StdoutPipe()
@@ -78,7 +77,10 @@ var _ = Describe("Events", func() {
 					}).Should(Equal(fmt.Sprintf("Allocated %d", memConsumedBytes)))
 
 					statsAfter := getStats(containerId)
-					Expect(statsAfter.Data.Memory.Stats.TotalRss).To(BeNumerically(">", initialRss))
+					goRuntimeOverhead := uint64(25 * 1024 * 1024)
+					expectedMemConsumedBytes := stats.Data.Memory.Stats.TotalRss + uint64(memConsumedBytes) + goRuntimeOverhead
+					threshold := 60 * 1024 * 1024
+					Expect(statsAfter.Data.Memory.Stats.TotalRss).To(BeNumerically("~", expectedMemConsumedBytes, threshold))
 					Expect(cmd.Wait()).To(Succeed())
 				})
 
