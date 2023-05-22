4302f985e6d0b575bc1024b4a1c5a174a8cff1c8
Amin Jamali <ajamali@vmware.com>
2023-05-22 17:40:53 +0000

    hcs{shim,} for newer versions of hscsshim
    Context: https://github.com/cloudfoundry/winc/commit/2f316e7778e0be441b630099f0dd57c9599c6144

--- integration/winc/events_test.go
+++ integration/winc/events_test.go
@@ -135,7 +135,7 @@ var _ = Describe("Events", func() {
 			stdOut, stdErr, err := helpers.Execute(cmd)
 			Expect(err).To(HaveOccurred(), stdOut.String(), stdErr.String())
 
-			Expect(stdErr.String()).To(ContainSubstring("hcsshim::OpenComputeSystem doesntexist"))
+			Expect(stdErr.String()).To(ContainSubstring("hcs::OpenComputeSystem doesntexist"))
 			Expect(stdErr.String()).To(ContainSubstring("the specified identifier does not exist"))
 		})
 	})
