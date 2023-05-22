2d0fe6efde64282552a6e507b94e310c78f0df87
Amin Jamali <ajamali@vmware.com>
2023-05-22 15:28:06 +0000

    throw error instead

--- bin/test.ps1
+++ bin/test.ps1
@@ -7,10 +7,10 @@ Configure-Winc-Network delete
 
 ginkgo.exe -p -r --race -keep-going --randomize-suites --fail-on-pending --skip-package winc-network,perf
 if ($LastExitCode -ne 0) {
-  Exit 1
+  throw "tests failed"
 }
 
 ginkgo.exe -r --race -keep-going --randomize-suites --fail-on-pending ./integration/perf ./integration/winc-network
 if ($LastExitCode -ne 0) {
-  Exit 1
+  throw "tests failed"
 }
